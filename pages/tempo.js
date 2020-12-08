71f5667fe33b27d076b0635a20d5739f59c55bf2
Thribs <thribsilva@gmail.com>
2020-12-07 23:32:11 -0300

    static

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-function Tempo() {
+function Tempo(props) {
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
 
@@ -14,8 +14,20 @@ function Tempo() {
             <a>Acessar página Sobre</a>
             </Link>
             <h4>{dynamicDateString} (dinâmico)</h4>
+            <h4>{staticDateString} (estático)</h4>
         </div>
     )
+}
+
+export function getStaticProps() {
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();
+
+    return {
+        props: {
+            staticDateString
+        }
+    }
 }
 
 export default Tempo;
