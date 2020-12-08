1f913d312455c7ad3296c484467edafe4d981673
Thribs <thribsilva@gmail.com>
2020-12-07 23:00:07 -0300

    testando Report

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ function Home() {
             <Link href="/sobre">
             <a>Acessar página Sobre</a>
             </Link>
+
+            <button href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Report</button>
         </div>
     )
 }
