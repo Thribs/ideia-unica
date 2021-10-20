27fc2430aa6f1526d369c5f9ae39fd259cf9386e
Thribs <thribsilva@gmail.com>
2021-10-20 20:54:31 -0300

    lorem

---
+++ pages/lorem.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+export default () => {
+    return (
+        <>
+        <h1>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</h1>
+        <p>Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.</p>
+
+        <h2>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, aperiam? Aut recusandae illo vitae animi sint maiores praesentium consequatur molestiae quas! Voluptatibus optio unde ex sint id in.</p>
+
+        <h2>Lorem ipsum dolor sit.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, facere! Aut, possimus officiis voluptatum praesentium est et illo?</p>
+
+        <h2>Lorem, ipsum.</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio dignissimos eos reiciendis veniam hic accusamus placeat, error laborum illo illum ipsum cumque consequatur, quibusdam repudiandae reprehenderit id praesentium enim! Consequatur ullam quae, odit dolorum tempora nostrum laudantium placeat deserunt magnam enim quos dolores commodi atque pariatur sint veniam fugit accusamus?</p>
+        
+        <h2>Lorem ipsum dolor sit.</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis quas cupiditate explicabo? Ipsa laudantium maxime vero aspernatur nesciunt! Sequi tenetur quisquam assumenda quaerat delectus, libero asperiores iusto facilis temporibus rem!</p>
+
+        <a href="https://google.com/search?q=bolo de cenoura" target="_blank">Lorem ipsum dolor sit amet consectetur.</a>
+        </>
+    )
+};
