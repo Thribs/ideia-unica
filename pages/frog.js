e2b6c47931503d9e87d7a44ffa8d30a8a9152f44
Thribs <thribsilva@gmail.com>
2021-10-21 21:48:29 -0300

    frog

--- pages/frog.js
+++ pages/frog.js
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function() {
     return (
-        <Image src="https://cdn.britannica.com/84/206384-050-00698723/Javan-gliding-tree-frog.jpg" alt="frog" width="1600" height="1145" />
+        <img src="https://cdn.britannica.com/84/206384-050-00698723/Javan-gliding-tree-frog.jpg" />
     )
 }
